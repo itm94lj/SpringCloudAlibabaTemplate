71fbf46ba33fe4b2a3736b3005de4adb5da69733
unknown <itm94lj@163.com>
2024-07-22 11:22:30 +0800

    使用spring-boot-maven-plugin插件来构造服务

---
+++ Demo/src/main/java/org/itm94lj/template/demo/config/RabbitMqConfig.java
@@ -0,0 +1,22 @@
+package org.itm94lj.template.demo.config;
+
+import com.rabbitmq.client.*;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PostConstruct;
+
+/**
+ * 消息队列相关的配置
+ */
+@Configuration
+@Slf4j
+public class RabbitMqConfig {
+
+
+    public void HelloWorld() {
+
+    }
+
+
+}
